302e37c52df1f55d9a8f27b73fbbdc368361a174
ayush4661 <66200721+ayush4661@users.noreply.github.com>
2021-08-26 16:19:14 +0530

    Update SceneChanger.cs

--- SceneChanger.cs
+++ SceneChanger.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;  
 public class SceneChanger: MonoBehaviour {  
     public void Scene1() {  
-        SceneManager.LoadScene("Acro");  
+        SceneManager.LoadScene("Acro"); //Switching to Acro scene.
     }  
     public void Scene2() {  
-        SceneManager.LoadScene("Astra");  
+        SceneManager.LoadScene("Astra");  //Switching to Astra scene.
     }  
     public void Scene3() {  
-        SceneManager.LoadScene("Zoo");  
+        SceneManager.LoadScene("Zoo");  //Switching to Zoo scene.
     } 
     public void Scene4() {  
-        SceneManager.LoadScene("StartMenu");  
+        SceneManager.LoadScene("StartMenu");  //Switching to StartMenu.
     }  
 } 
